d3731e62a17978f693aa88b7e3803f17983e0bbf
Peter Czanik <peter@czanik.hu>
2018-09-17 18:10:09 +0200

    Addressed review recommendations: - removed boilerplate LogDestination object - added printdebug() to print debug messages - merged __init__() into init() - renamed decoded to decoded_msg - self.is_opened set to False if open() or send() fails
    Signed-off-by: Peter Czanik <peter@czanik.hu>

--- mqtt_dest.py
+++ mqtt_dest.py
@@ -7,48 +7,21 @@ host, port and topic are mandatory parameters
 
 import paho.mqtt.client as mqtt
 
-class LogDestination(object):
-    """
-    boilerplate code
-    """
-    def open(self):
-        """Open a connection to the target service
 
-        Should return False if opening fails"""
-        return True
-
-    def close(self):
-        """Close the connection to the target service"""
-        pass
-
-    def is_opened(self):
-        """Check if the connection to the target is able to receive messages"""
-        return True
-
-    def init(self, options):
-        """This method is called at initialization time
-
-        Should return false if initialization fails"""
-        return True
-
-    def deinit(self):
-        """This method is called at deinitialization time"""
-        pass
-
-    def send(self, msg):
-        """Send a message to the target service
-
-        It should return True to indicate success, False will suspend the
-        destination for a period specified by the time-reopen() option."""
-        return True
-
-class MqttDestination(LogDestination):
+class MqttDestination(object):
     """
     the MqttDestination class, reference this from syslog-ng.conf
     """
-    def __init__(self):
+
+    def printdebug(self, msg):
+        """prints debug message if debug is enabled"""
+        if self.debug > 0:
+            print(msg)
+
+    def init(self, options):
         """
-        basic initialization
+        initializing MQTT parameters, fails if mandatory parameters
+        are not available, or not in the right format
         """
         self.host = None
         self.port = None
@@ -57,12 +30,6 @@ class MqttDestination(LogDestination):
         self.debug = 0
         self.qos = 0
         self.mqttc = mqtt.Client("sng_mqtt")
-
-    def init(self, options):
-        """
-        initializing MQTT parameters, fails if mandatory parameters
-        are not available, or not in the right format
-        """
         try:
             print('MQTT destination options: ' + str(options))
             self.host = options["host"]
@@ -88,11 +55,10 @@ class MqttDestination(LogDestination):
             self.mqttc.connect(self.host, self.port)
             self.mqttc.loop_start()
             self._is_opened = True
-            if self.debug > 0:
-                print('MQTT destination: opened...')
+            self.printdebug('MQTT destination: opened...')
         except:
-            if self.debug > 0:
-                print('MQTT destination: opening ' + self.host + ' at ' + str(self.port) + 'failed...')
+            self.printdebug('MQTT destination: opening ' + self.host + ' at ' + str(self.port) + 'failed...')
+            self._is_opened = False
             return False
         return True
 
@@ -101,8 +67,7 @@ class MqttDestination(LogDestination):
         close the connection
         """
         self.mqttc.disconnect()
-        if self.debug > 0:
-            print('MQTT destination: closing connection to ' + self.host + ' at ' + str(self.port))
+        self.printdebug('MQTT destination: closing connection to ' + self.host + ' at ' + str(self.port))
         self._is_opened = False
 
     def deinit(self):
@@ -113,16 +78,14 @@ class MqttDestination(LogDestination):
         """
         send the message
         """
-        decoded = msg['MESSAGE'].decode('utf-8')
+        decoded_msg = msg['MESSAGE'].decode('utf-8')
         try:
-            if self.debug > 0:
-                print('MQTT destination: before sending')
-            self.mqttc.publish(self.topic, decoded, qos=self.qos)
-            if self.debug > 0:
-                print('MQTT destination: after sending')
+            self.printdebug('MQTT destination: before sending')
+            self.mqttc.publish(self.topic, decoded_msg, qos=self.qos)
+            self.printdebug('MQTT destination: after sending')
         except:
-            if self.debug > 0:
-                print('MQTT destination: sending to topic ' + self.topic + ' failed...')
+            self.printdebug('MQTT destination: sending to topic ' + self.topic + ' failed...')
+            self._is_opened = False            
             return False
         return True
 
@@ -130,7 +93,7 @@ class MqttDestination(LogDestination):
 # code to test the MqttDestination outside of syslog-ng
 print('starting up...')
 bla = MqttDestination()
-bla.init(options=dict(host="127.0.0.1",port="1883",topic="syslog/warn"))
+bla.init(options=dict(host="172.16.167.132",port="1883",topic="syslog/warn",debug="1"))
 bla.open()
 bla.send(msg=dict(MESSAGE=b"It's working..."))
 bla.send(msg=dict(MESSAGE=b"Still working..."))
